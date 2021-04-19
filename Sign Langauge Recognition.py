70daab3a13fc626201ccb14ae8af6e1f3f96af02
ShoRaz~ <shorajmanandhar@hotmail.com>
2021-04-19 15:54:36 -0700

    Selecting L/R Hand & using imshow to display image in try folder

--- Sign Langauge Recognition.py
+++ Sign Langauge Recognition.py
@@ -8,13 +8,16 @@ def load_images(folder):
     images = [] 
     labels = []
     for filename in os.listdir(folder):
-#         print(filename)
+        # print(filename)
         image = io.imread(os.path.join(folder, filename))
         if image is not None:
-            images.append(image)
-            labels.append(filename[-6])
+            if filename[-5] == 'L':
+                images.append(image)
+                # images.append(image)
+                labels.append(filename[-6])
             # display_image(image)
     images =  np.asarray(images)
+    labels = np.asarray(labels)
     return images, labels
   
 
@@ -24,21 +27,24 @@ def display_image(img):
     plt.show()  
 
 
-def import_data():
-    X_train = []  
-    X_test = []   
-    Y_train = []     
-    Y_test = []     
-
-
 def main():
-    dataset,labels = load_images('./data/train/')
-    # dataset = load_images('./try/')
-    print(type(dataset))
-    print(dataset.shape)
-    # print(dataset)
-    print(labels)
+    # X_train, X_test = load_images('./data/train/')
+    # Y_train, Y_test = load_images('./data/test/')
 
+    # Testing the image with one or two image in a folder
+    X_train, X_test = load_images('./try/')
+
+    print("X training dataset: ", X_train.shape)
+    print("X testing dataset: ", X_test.shape)
+    # print("Y training dataset: ", Y_train.shape)
+    # print("Y testing dataset: ", Y_test.shape)
+
+    fig, axes = plt.subplots(2, 2, figsize=(8,8))
+    ax = axes.ravel()
+    for i in range (len(X_train)):  # X_train
+        ax[i].imshow(X_train[i])
+    fig.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
     main()
